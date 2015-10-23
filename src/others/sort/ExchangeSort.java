d8a52a2ae1107d69cd3945c947f162a72cb4e4f5
yangqingwei <824157351@qq.com>
2015-10-23 16:15:15 +0800

    快排

--- src/others/sort/ExchangeSort.java
+++ src/others/sort/ExchangeSort.java
@@ -13,9 +13,13 @@ public class ExchangeSort {
 	public static void main(String[] args) {
 		int[] arr = {2, 3, 8, 4, 1};
 		ExchangeSort ex = new ExchangeSort();
-		ex.bubbleSort(arr);
+//		ex.bubbleSort(arr);
+//		Print.printArray(arr);
+		
+		ex.quickSort(arr, 0, arr.length - 1);
 		Print.printArray(arr);
 	}
+
 
 	/**
 	 * 冒泡排序
@@ -34,4 +38,44 @@ public class ExchangeSort {
 		}
 	}
 	
+	/**
+	 * 快排
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 */
+	public void quickSort(int[] arr, int low, int high) {
+		if(low < high) {
+			int privotLoc = parition(arr, low, high);
+			quickSort(arr, low, privotLoc - 1);
+			quickSort(arr, privotLoc + 1, high);
+		}
+	}
+	
+	/**
+	 * 将arr划分成两个部分，基准数左侧小于基准数，右侧大于它，并返回基准数位置
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	private int parition(int[] arr, int low, int high) {
+		int privotKey = arr[low];
+		while(low < high) {
+			while(low < high && arr[high] >= privotKey) high--;
+			swap(arr, low, high);
+			
+			while(low < high && arr[low] <= privotKey) low++;
+			swap(arr, low, high);
+		}
+		return low;
+	}
+	
+	private void swap(int[] arr, int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
 }
